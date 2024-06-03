62f96dc073c3155aa99c6e885d9dab0af5584bc7
unknown <bijontalukder1247@gmail.com>
2024-06-03 23:53:33 +0600

    practice

--- src/components/CategoryNav.tsx
+++ src/components/CategoryNav.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 
 function CategoryNav() {
   return (
-    <div className="hidden lg:block">
+    <>
+     <div className="hidden lg:block">
         <div className='container'>
             <div className='flex w-fit gap-10 mx-auto font-medium py-4 text-black'> 
             
@@ -37,6 +38,23 @@ function CategoryNav() {
             </div>
         </div>
     </div>
+    
+    <div className="drawer drawer-end">
+  <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+  <div className="drawer-content">
+    {/* Page content here */}
+    <label htmlFor="my-drawer-4" className="drawer-button btn btn-primary">Open drawer</label>
+  </div> 
+  <div className="drawer-side">
+    <label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay"></label>
+    <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+      {/* Sidebar content here */}
+      <li><a>Sidebar Item 1</a></li>
+      <li><a>Sidebar Item 2</a></li>
+    </ul>
+  </div>
+</div></>
+   
   )
 }
 
